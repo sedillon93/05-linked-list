0ba27d5e1968310a134f425dbd77275ff90229c5
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-03 10:22:33 -0800

    establish file system, write 3 tests, write started code for find method

---
+++ __test__/model.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe(`model.js`, () => {
+  test(`LinkedList's find method should return the present node if its value matches the value given as the argument`, () => {
+    expect().toBe();
+  });
+  test(`LinkedList's find method should return an error if it reaches the final node`, () => {
+    expect().toThrow();
+  });
+  test(`LinkedList's find method should run again on the next node if the value has not been found yet and the next node is not null`, () => {
+    expect().toBe();
+  });
+})
