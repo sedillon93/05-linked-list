03795521a84263f253d9d6f52053e9bbefbc9892
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-03 21:18:03 -0800

    trying to create linked list to test methods

--- __test__/model.test.js
+++ __test__/model.test.js
@@ -1,13 +1,13 @@
 'use strict';
 
+const find = require(`../model/find`);
+console.log(list);
+
 describe(`model.js`, () => {
-  test(`LinkedList's find method should return the present node if its value matches the value given as the argument`, () => {
+  test(`LinkedList's find method should return node if its value matches the value given as the argument`, () => {
     expect().toBe();
   });
-  test(`LinkedList's find method should return an error if it reaches the final node`, () => {
-    expect().toThrow();
-  });
-  test(`LinkedList's find method should run again on the next node if the value has not been found yet and the next node is not null`, () => {
-    expect().toBe();
+  test(`LinkedList's find method should return null if it reaches the final node`, () => {
+    expect().toEqual(null);
   });
 });
--- model/find.js
+++ model/find.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function LinkedList(){}
-//function LinkedList...
+// function LinkedList(){}
+// function LinkedList...
 //vinicio - classes are not hoisted :p
 class LinkedList{
   //new LinkedList()...
@@ -10,18 +10,20 @@ class LinkedList{
     this.next = null;
   }
 
-  // append(node){
-  //   if(!(node instanceof LinkedList))
-  //     throw new TypeError('<node> should be an instance of LinkedList');
-  //
-  //   // vinicio - we know we are at the last element if there is no next
-  //   if(!this.next)
-  //     this.next = node;
-  //   else
-  //     this.next.append(node);
-  //
-  //   return this;
-  // }
+  append(node){
+    if(!(node instanceof LinkedList))
+      console.log(node, `is the node`);
+      console.log(this, `is 'this'`);
+      throw new TypeError('<node> should be an instance of LinkedList');
+
+    // vinicio - we know we are at the last element if there is no next
+    if(!this.next)
+      this.next = node;
+    else
+      this.next.append(node);
+
+    return this;
+  }
 
   //TODO : Homework
   find(value){
@@ -30,29 +32,41 @@ class LinkedList{
     }
 
     else if(this.next = null){  //if this is the last node, the value isn't in the list
-      throw new Error(`no such value exists`);
+      return null;
     }
 
-    else {                      //go to the next node and check that
+    else {                      //go to the next node and check that value
       this.next.find(value)
     };
   }
 
   //vinicio - remove has( intentionally n_o), a bug. Can you find it?
-//   remove(node){
-//     if(!(node instanceof LinkedList))
-//       throw new TypeError('<node> should be an instance of LinkedList');
-//
-//     if(!this.next)
-//       return this;
-//     if(this.next === node){
-//       //vinicio - here we know we need to remove the NEXT node
-//       this.next = this.next.next;
-//     } else {
-//       this.next.remove(node);
-//     }
-//     return this;
-//   }
-// }
+  remove(node){
+    if(!(node instanceof LinkedList))
+      throw new TypeError('<node> should be an instance of LinkedList');
+
+    //if the very first node is the one we want to delete, replace it with the next node (is this how you would do that?)
+    // if(this === node){
+    //   this = this.next;
+    // }
+
+    if(!this.next)
+      return this;
+    if(this.next === node){
+      //vinicio - here we know we need to remove the NEXT node
+      this.next = this.next.next;
+    } else {
+      this.next.remove(node);
+    }
+    return this;
+  }
+}
+
+let list = new LinkedList(2);
+console.log(list);
+list.append(10);
+console.log(list);
+list.append(5);
+list.append(2);
 
 module.exports = LinkedList;
